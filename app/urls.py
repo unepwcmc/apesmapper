3403e219aca6f652b16a1599724e82eda0ffb70a
javi <qualopec@gmail.com>
2011-11-17 08:51:33 +0100

    ie6 redirect

--- app/urls.py
+++ app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     (r'^tool$', direct_to_template, {'template':'index.html'}),
     (r'^data$', direct_to_template, {'template':'data.html'}),
     (r'^nojavascript$', direct_to_template, {'template':'nojs.html'}),
+    (r'^ie6$', direct_to_template, {'template':'ie6.html'}),
     (r'^api/v0/', include('carbontool.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
