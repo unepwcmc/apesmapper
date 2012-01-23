e7b3c1af4f6da82e7e7cc3589e5889407326b722
James Cox <th3james@gmail.com>
2012-01-23 11:14:13 +0000

    Add some more fake data, make graph reflect that

--- app/assets/js/views/slide_filters.js
+++ app/assets/js/views/slide_filters.js
@@ -30,15 +30,16 @@ App.views.SlideFilters = Backbone.View.extend({
   },
 
   addAll: function() {
+    jQuery("#results-table tbody").empty();
     this.sites.each(this.addOne);
   },
 
   addOne: function(site) {
     var colors = ['#F5F5DC', '#0000FF', '#0095B6', '#8A2BE2', '#CD7F32', '#964B00', '#702963', '#960018', '#DE3163', '#007BA7', '#F7E7CE', '#7FFF00', '#6F4E37', '#B87333', '#F88379', '#DC143C', '#00FFFF', '#EDC9AF'];
-    window.bubbleChart.addBubble(site.get('centre_point_x'), site.get('centre_point_y'), site.get('area'), colors[Math.floor(Math.random()*colors.length)], site.get('name'));
+    window.bubbleChart.addBubble(site.get('state')*100, site.get('pressure')*100, site.get('area'), colors[Math.floor(Math.random()*colors.length)], site.get('name'));
 
     var view = new App.views.FilterView({model : site});
-    jQuery("#results-table tbody").html(view.render().el);
+    jQuery("#results-table tbody").append(view.render().el);
   },
 
   render: function() {
