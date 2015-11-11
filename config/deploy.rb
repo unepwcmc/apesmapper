d407f3f4821660367753cdaa566da9ffc96572b6
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-11-11 10:39:07 +0000

    changed deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.4.0'
 
 set :application, 'apesmapper'
 set :repo_url, 'git@github.com:unepwcmc/apesmapper.git'
-set :branch, 'LinodeCap3Deploy'
+set :branch, 'master'
 
 set :deploy_user, 'wcmc'
 set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
